2d696086ca3f1e49015c6732ef8c1328c07a5ce9
chrisryda <chrisryda@hotmail.com>
2025-04-30 09:58:34 +0200

    rep1 starting

---
+++ rep/rep1/build/CMakeFiles/ps1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/crd/Documents/y5s2/compcon-TDT4205/rep/rep1/src/driver.c" "CMakeFiles/ps1.dir/src/driver.c.o" "gcc" "CMakeFiles/ps1.dir/src/driver.c.o.d"
+  "" "ps1" "gcc" "CMakeFiles/ps1.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ rep/rep1/build/CMakeFiles/ps1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ps1.dir/link.d"
+  "CMakeFiles/ps1.dir/src/driver.c.o"
+  "CMakeFiles/ps1.dir/src/driver.c.o.d"
+  "ps1"
+  "ps1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ps1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
