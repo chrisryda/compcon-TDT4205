bebf4d482262aa9c857a8848c26c1fa7d35ec85e
chrisryda <chrisryda@hotmail.com>
2025-05-01 13:04:47 +0200

    easy done

---
+++ rep/rep3_llvmir/easy.c
@@ -0,0 +1,35 @@
+/*
+define i32 @ssa_function(i32 %a) {
+    1:
+      %cmp.0 = icmp slt i32 %a, 0                               // if a < 0       --> cmp.0 = 1
+      br i1 %cmp.0, label %ifTrue, label %ifFalse               // if cmp.0 == 1  --> jump to %ifTrue 
+    
+    ifTrue: ; preds = %1
+      %sub.0 = sub nsw i32 0, %a                                // sub.0 = 0 - a
+      br label %ifEnd
+    
+    ifFalse: ; preds = %1
+      br label %ifEnd
+    
+    ifEnd: ; preds = %ifTrue, %ifFalse
+      %phi.0 = phi i32 [ %sub.0, %ifTrue ], [ %a, %ifFalse ]
+      ret i32 %phi.0
+}
+*/
+
+
+int abs(int a)
+{
+    if (a < 0 ) 
+    { 
+        return -a; 
+    }
+    else 
+    { 
+        return a; 
+    }
+}
+
+int main() {
+    return 0;
+}
