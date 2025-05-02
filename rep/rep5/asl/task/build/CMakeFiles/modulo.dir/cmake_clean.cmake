4cfe8c2ed417246f76143513a893eb40e035799f
chrisryda <chrisryda@hotmail.com>
2025-05-02 17:30:09 +0200

    trying asm

---
+++ rep/rep5/asl/task/build/CMakeFiles/modulo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/modulo.dir/src/modulo.S.o"
+  "modulo"
+  "modulo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM)
+  include(CMakeFiles/modulo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ rep/rep5/asl/task/build/CMakeFiles/modulo.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for modulo.
+# This may be replaced when dependencies are built.
