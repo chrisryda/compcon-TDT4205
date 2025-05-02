3d1d2999900e1a589f9a989522c54169fa72fce1
chrisryda <chrisryda@hotmail.com>
2025-05-02 13:14:47 +0200

    init rep4

---
+++ rep/rep4/build/CMakeFiles/vslc.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vslc.dir/link.d"
+  "CMakeFiles/vslc.dir/parser.c.o"
+  "CMakeFiles/vslc.dir/parser.c.o.d"
+  "CMakeFiles/vslc.dir/scanner.c.o"
+  "CMakeFiles/vslc.dir/scanner.c.o.d"
+  "CMakeFiles/vslc.dir/src/graphviz_output.c.o"
+  "CMakeFiles/vslc.dir/src/graphviz_output.c.o.d"
+  "CMakeFiles/vslc.dir/src/symbol_table.c.o"
+  "CMakeFiles/vslc.dir/src/symbol_table.c.o.d"
+  "CMakeFiles/vslc.dir/src/symbols.c.o"
+  "CMakeFiles/vslc.dir/src/symbols.c.o.d"
+  "CMakeFiles/vslc.dir/src/tree.c.o"
+  "CMakeFiles/vslc.dir/src/tree.c.o.d"
+  "CMakeFiles/vslc.dir/src/vslc.c.o"
+  "CMakeFiles/vslc.dir/src/vslc.c.o.d"
+  "parser.c"
+  "parser.h"
+  "scanner.c"
+  "scanner.h"
+  "vslc"
+  "vslc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vslc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
